8f5b937d05b6646b9d6849ce06b3c8615a1a5d4f
Zhang Lei <942471854@qq.com>
2019-10-09 01:22:29 +0800

    Revert "完成测试"
    This reverts commit 957935d1559505f96c42b65badbef739cac1b1c0.

--- Interpreter/src/shouldfailcase/error2.c
+++
@@ -1,6 +0,0 @@
-// 变量名错误(不能以数字和符号开头)
-int main() {
-    int  1a;
-    bool  /b;
-    return 0;
-}
--- Interpreter/src/testcase/while.c
+++
@@ -1,9 +0,0 @@
-int main() {
-    int a = 0;
-    int n = 100;
-    while (n > 0) {
-        a += n;
-        n--;
-    }
-    return 0;
-}
